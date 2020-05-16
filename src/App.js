b09c6bbc9288e4af797ced19423fe5376e88151f
Greg Lim <limjunqu@gmail.com>
2020-05-16 19:58:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer} from 'react';
+import ToDoList from './ToDoList'
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
+const todosInitialState = { 
+  todos:[{ id:1, text: "finishing writing hooks chapter"},
+    { id:2, text: "play with kids"},
+    { id:3, text: "read bible"}
+  ]
+};
+
+export const TodosContext = React.createContext()
+
+function App (){
+  const [state, dispatch] = useReducer(todosReducer,todosInitialState)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <TodosContext.Provider value={{state,dispatch}}>      
+      <ToDoList />
+    </TodosContext.Provider>    
+  )
+}
+
+function todosReducer(state, action){ 
+  switch(action.type){     
+    case 'add':
+      const newToDo = {id: uuidv4(), text: action.payload}
+      const addedToDos = [...state.todos,newToDo]
+      return {...state,todos:addedToDos}
+    case 'delete':
+      const filteredTodoState = state.todos.filter( todo => todo.id !== action.payload.id)
+      return {...state, todos: filteredTodoState}
+    case 'edit':   
+      const updatedToDo = {...action.payload} 
+      const updatedToDoIndex = state.todos.findIndex(t => t.id === action.payload.id)
+      const updatedToDos = [
+        ...state.todos.slice(0,updatedToDoIndex),
+        updatedToDo,
+        ...state.todos.slice(updatedToDoIndex + 1)
+      ];
+      return {...state, todos: updatedToDos}      
+    default:
+      return todosInitialState
+  }
 }
 
 export default App;
